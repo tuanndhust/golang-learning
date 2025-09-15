27ad1b8d11fea062e0dc1e8ac8827358fbffc978
tuanndhust <nguyentuan2701hd@gmail.com>
2025-09-15 17:31:06 +0700

    example

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,11 +1,14 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var investmentAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
 	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
-	// fmt.Print("Hello world")
+	fmt.Println(futureValue)
 }
