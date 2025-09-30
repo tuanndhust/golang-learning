afc9061d61df2f72bbfd11aacb6abada9a2f23ca
tuanndhust <nguyentuan2701hd@gmail.com>
2025-09-30 17:18:16 +0700

    parallel

---
+++ 10.Price_PracticeP/prices/prices.go
@@ -0,0 +1,40 @@
+package prices
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("could not open file")
+		fmt.Println(err)
+		return
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+}
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+	fmt.Println(result)
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
